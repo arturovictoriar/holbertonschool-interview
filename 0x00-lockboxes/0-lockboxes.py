b6bd248e73b925b8f5b49becca8ed7a5961f7f03
arvicrin <1231@holbertonschool.com>
2020-07-19 04:30:02 +0000

    Can we open all boxes? yes or not iterative

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,15 +7,13 @@ def canUnlockAll(boxes):
     if not lenght_boxes:
         return False
 
-    def list_open_boxes(j, keys, lenght_boxes):
-        keys.append(j)
-        if len(keys) == lenght_boxes:
-            return keys
-        all_keys = keys
-        for i in boxes[j]:
-            if i not in keys and i < lenght_boxes:
-                all_keys = list_open_boxes(i, keys, lenght_boxes)
-        return all_keys
+    def list_open_boxes(first_box_open, all_keys_gotten, lenght_boxes):
+        all_keys_gotten.append(first_box_open)
+        for key_box_gotten in all_keys_gotten:
+            for key_in_box in boxes[key_box_gotten]:
+                if key_in_box not in all_keys_gotten and key_in_box < lenght_boxes:
+                    all_keys_gotten.append(key_in_box)
+        return all_keys_gotten
 
     if len(list_open_boxes(0, [], lenght_boxes)) == lenght_boxes:
         return True
