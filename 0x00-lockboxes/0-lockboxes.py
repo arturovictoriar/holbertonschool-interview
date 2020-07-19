b4e74df9fdfbfe8750de091b418177b38e5084cf
arvicrin <1231@holbertonschool.com>
2020-07-19 04:01:06 +0000

    Make auto-explicative code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,17 +5,16 @@ def canUnlockAll(boxes):
     """open all boxes"""
     lenght_boxes = len(boxes)
 
-    def hola(j, keys, lenght_boxes):
-        keys[j] = 1
+    def list_open_boxes(j, keys, lenght_boxes):
+        keys.append(j)
         if len(keys) == lenght_boxes:
             return keys
         all_keys = keys
         for i in boxes[j]:
-            if (i not in keys or keys[i] == 0) and i < lenght_boxes:
-                j = i
-                all_keys = hola(j, keys, lenght_boxes)
+            if i not in keys and i < lenght_boxes:
+                all_keys = list_open_boxes(i, keys, lenght_boxes)
         return all_keys
 
-    if len(hola(0, {}, lenght_boxes)) == lenght_boxes:
+    if len(list_open_boxes(0, [], lenght_boxes)) == lenght_boxes:
         return True
     return False
