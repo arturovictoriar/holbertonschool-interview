77a6b5e8d0d7194c98926193051fee38cc6869e4
arvicrin <1231@holbertonschool.com>
2020-08-10 05:00:52 +0000

    Iterative solution of the minoperation function

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,43 +1,21 @@
 #!/usr/bin/python3
 """
-    Get the minumun operation recursive
-    it take a lot time with numbers bigger than 40
+    Get the minumun operation iterative
+    it no take a lot time
 """
 
 
-def min_c(m, n, i, c):
-    """Get the minum operation recursive"""
-    if m > n or i > n:
-        return -1
-    elif m == n:
-        return i
-    min1 = min_c(m + c, n, i + 1, c)
-    min2 = min_c(m, n, i + 1, m)
-
-    if min1 == -1 and min2 != -1:
-        return min2
-    if min2 == -1 and min1 != -1:
-        return min1
-    if min1 == -1 and min2 == -1:
-        return -1
-    if min1 > min2:
-        return min2
-    if min2 > min1:
-        return min1
-    return min1
-
-
 def minOperations(n):
-    """Get the minum operation recursive"""
+    """Get the minum operation iterative"""
     if n <= 1:
         return 0
-    i = 0
-    m = 1
-    c = m
-    i += 1
-    m = m + c
-    i += 1
-    min = min_c(m, n, i, c)
-    if min == -1:
-        return 0
-    return min
+    number = n
+    div = 2
+    min_oper = 0
+    while number > 1:
+        if number % div == 0:
+            number /= div
+            min_oper += div
+        else:
+            div += 1
+    return min_oper
