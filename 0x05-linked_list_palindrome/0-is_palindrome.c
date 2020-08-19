e3f246d58c6f4e1db8359836a7cc285996fc39af
arvicrin <1231@holbertonschool.com>
2020-08-19 18:05:27 +0000

    Fix betty style

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -7,23 +7,23 @@
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *copy_head = *head;
-    int len_head = 0, array_head[3000], i = 0;
+	listint_t *copy_head = *head;
+	int len_head = 0, array_head[3000], i = 0;
 
-    if (!*head)
-        return (1);
+	if (!*head)
+		return (1);
 
-    while (copy_head)
-    {
-        array_head[len_head] = copy_head->n;
-        len_head++, copy_head = copy_head->next;
-    }
+	while (copy_head)
+	{
+		array_head[len_head] = copy_head->n;
+		len_head++, copy_head = copy_head->next;
+	}
 
-    for (i = 0; i < len_head; i++)
-        if (array_head[i] != array_head[len_head - 1 - i] || i > len_head / 2)
-            break;
+	for (i = 0; i < len_head; i++)
+		if (array_head[i] != array_head[len_head - 1 - i] || i > len_head / 2)
+			break;
 
-    if (i <= len_head / 2)
-        return (0);
-    return (1);
+	if (i <= len_head / 2)
+		return (0);
+	return (1);
 }
