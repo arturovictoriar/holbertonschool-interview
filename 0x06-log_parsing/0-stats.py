07d8a44b78c4bcd736ed1614783e386b2e175998
arvicrin <1231@holbertonschool.com>
2020-08-30 22:34:59 +0000

    Remove first comment,

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-"""Print statistics from html request"""
-
 import sys
 
-general_stat = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-size_file = 0
 
+def print_stats(number_status, total_size):
+    """
+    Print the stats of the request
+    """
+    print("File size: {}".format(total_size))
+    for status_req, num_status in number_status.items():
+        if num_status != 0:
+            print("{}: {}".format(status_req, num_status))
+        number_status[status_req] = 0
 
-def print_stats():
-    """Print the request statistics"""
-    print("File size: {}".format(size_file))
-    for status_req in sorted(general_stat.keys()):
-        if general_stat[status_req]:
-            print("{}: {}".format(status_req, general_stat[status_req]))
-        general_stat[status_req] = 0
-
+number_status = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0
+}
 i = 1
+total_size = 0
+
 try:
     for line in sys.stdin:
         parse_line = line.split(" ")
-        general_stat[parse_line[7]] += 1
-        size_file += int(parse_line[8])
+        total_size += int(parse_line[8])
+        number_status[parse_line[7]] += 1
         if i % 10 == 0:
-            print_stats()
-            size_file = 0
+            print_stats(number_status, total_size)
+            total_size = 0
         i += 1
 except KeyboardInterrupt:
-    print_stats()
+    print_stats(number_status, total_size)
     raise
