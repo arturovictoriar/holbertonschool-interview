cc7458f8e9295da847f08842cdcbdbc3364adfa4
arvicrin <1231@holbertonschool.com>
2020-08-30 21:15:25 +0000

    Change the way to use ctrl-c

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,36 +12,31 @@
     <status code>: <number>
 """
 
-import signal
 import sys
 
 general_stat = {}
+size_file = 0
 
 
 def print_stats():
     """Print the request statistics"""
-    msg_to_print = ''
-    file_size = 0
+    print("File size: {}".format(size_file))
     for status_request in sorted(general_stat.keys()):
         number_status = general_stat[status_request]["number_status"]
-        msg_to_print += "{}: {}\n".format(status_request, number_status)
-        file_size += general_stat[status_request]["file_size"]
-    print("File size: {}\n{}".format(file_size, msg_to_print), end="")
+        print("{}: {}".format(status_request, number_status))
 
-
-def signal_handler(signal, frame):
-    """Print the stats when ctrl-c interruption will be activated"""
+try:
+    i = 1
+    for line in sys.stdin:
+        parse_line = line.split(' ')
+        if parse_line[7] not in general_stat:
+            general_stat[parse_line[7]] = {"number_status": 0, "file_size": 0}
+        general_stat[parse_line[7]]["number_status"] += 1
+        general_stat[parse_line[7]]["file_size"] += int(parse_line[8])
+        size_file += int(parse_line[8])
+        if i % 10 == 0:
+            print_stats()
+        i += 1
+except KeyboardInterrupt:
     print_stats()
-
-signal.signal(signal.SIGINT, signal_handler)
-
-i = 0
-for line in sys.stdin:
-    parse_line = line.split()
-    if parse_line[7] not in general_stat:
-        general_stat[parse_line[7]] = {"number_status": 0, "file_size": 0}
-    general_stat[parse_line[7]]["number_status"] += 1
-    general_stat[parse_line[7]]["file_size"] += int(parse_line[8], base=10)
-    if (i + 1) % 10 == 0:
-        print_stats()
-    i += 1
+    raise
