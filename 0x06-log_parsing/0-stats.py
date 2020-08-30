a9d841e702811cafd6be00c3d3448fd54539f802
arvicrin <1231@holbertonschool.com>
2020-08-30 23:23:51 +0000

    Fix condition when there is a request that no exit in the dict stats

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,7 +13,14 @@
 """
 import sys
 
-general_stat = {}
+general_stat = {"200": {"number_status": 0, "file_size": 0},
+                "301": {"number_status": 0, "file_size": 0},
+                "400": {"number_status": 0, "file_size": 0},
+                "401": {"number_status": 0, "file_size": 0},
+                "403": {"number_status": 0, "file_size": 0},
+                "404": {"number_status": 0, "file_size": 0},
+                "405": {"number_status": 0, "file_size": 0},
+                "500": {"number_status": 0, "file_size": 0}}
 
 
 def print_stats():
@@ -22,7 +29,8 @@ def print_stats():
     file_size = 0
     for status_request in sorted(general_stat.keys()):
         number_status = general_stat[status_request]["number_status"]
-        msg_to_print += "{}: {}\n".format(status_request, number_status)
+        if number_status:
+            msg_to_print += "{}: {}\n".format(status_request, number_status)
         file_size += general_stat[status_request]["file_size"]
     print("File size: {}".format(file_size))
     print(msg_to_print, end="")
@@ -34,12 +42,13 @@ def main():
     for line in sys.stdin:
         parse_line = line.split(" ")
         if parse_line[7] not in general_stat:
-            general_stat[parse_line[7]] = {"number_status": 0, "file_size": 0}
+            continue
         general_stat[parse_line[7]]["number_status"] += 1
         general_stat[parse_line[7]]["file_size"] += int(parse_line[8])
         if i % 10 == 0:
             print_stats()
         i += 1
+
 
 if __name__ == "__main__":
     try:
