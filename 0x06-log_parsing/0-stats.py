e48f7df9c967c0e2fb222efa16bd5af3966b4291
arvicrin <1231@holbertonschool.com>
2020-08-30 20:43:23 +0000

    Fix the file_size sum

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -44,7 +44,6 @@ def main():
             general_stat[parse_line[7]] = {"number_status": 0, "file_size": 0}
         general_stat[parse_line[7]]["number_status"] += 1
         general_stat[parse_line[7]]["file_size"] += int(parse_line[8], base=10)
-        print(int(parse_line[8], base=10))
         if (i + 1) % 10 == 0:
             print_stats()
         i += 1
