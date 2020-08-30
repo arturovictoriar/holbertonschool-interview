38cc0a74c763737ee5152cd0ab4f91b63fe72f8c
arvicrin <1231@holbertonschool.com>
2020-08-30 23:09:18 +0000

    Roll back, use try and except and add last print when there is no more lines

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,36 +1,49 @@
 #!/usr/bin/python3
-'''Print stats from html request'''
+"""
+    According this input request:
+    <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+    <status code> <file size>
+    Print the request statistics in the next way:
+    File size: <total size>
+    <status code>: <number>
+    .
+    .
+    .
+    <status code>: <number>
+"""
 import sys
 
-if __name__ == "__main__":
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+general_stat = {}
 
-    def print_stats():
-        '''Print status'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
 
-    def get_status_file_size(line):
-        '''Get the status and file_size'''
-        try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
-        except:
-            pass
+def print_stats():
+    """Print the request statistics"""
+    msg_to_print = ''
+    file_size = 0
+    for status_request in sorted(general_stat.keys()):
+        number_status = general_stat[status_request]["number_status"]
+        msg_to_print += "{}: {}\n".format(status_request, number_status)
+        file_size += general_stat[status_request]["file_size"]
+    print("File size: {}".format(file_size))
+    print(msg_to_print, end="")
+
+
+def main():
+    """Read a line and save request information"""
     i = 1
+    for line in sys.stdin:
+        parse_line = line.split(" ")
+        if parse_line[7] not in general_stat:
+            general_stat[parse_line[7]] = {"number_status": 0, "file_size": 0}
+        general_stat[parse_line[7]]["number_status"] += 1
+        general_stat[parse_line[7]]["file_size"] += int(parse_line[8])
+        if i % 10 == 0:
+            print_stats()
+        i += 1
+
+if __name__ == "__main__":
     try:
-        for line in sys.stdin:
-            get_status_file_size(line)
-            if i % 10 == 0:
-                print_stats()
-            i += 1
+        main()
     except KeyboardInterrupt:
         print_stats()
         raise
