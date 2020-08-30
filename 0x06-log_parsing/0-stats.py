c7d1a10f81c76011fa885f4ed0c5f81c447322ed
arvicrin <1231@holbertonschool.com>
2020-08-30 20:52:04 +0000

    Remove execute main only

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -24,8 +24,7 @@ def print_stats():
     file_size = 0
     for status_request in sorted(general_stat.keys()):
         number_status = general_stat[status_request]["number_status"]
-        msg_to_print += "{}: {}".format(status_request, number_status)
-        msg_to_print += '\n'
+        msg_to_print += "{}: {}\n".format(status_request, number_status)
         file_size += general_stat[status_request]["file_size"]
     print("File size: {}\n{}".format(file_size, msg_to_print), end="")
 
@@ -34,20 +33,15 @@ def signal_handler(signal, frame):
     """Print the stats when ctrl-c interruption will be activated"""
     print_stats()
 
+signal.signal(signal.SIGINT, signal_handler)
 
-def main():
-    """Read a line and save request information"""
-    i = 0
-    for line in sys.stdin:
-        parse_line = line.split()
-        if parse_line[7] not in general_stat:
-            general_stat[parse_line[7]] = {"number_status": 0, "file_size": 0}
-        general_stat[parse_line[7]]["number_status"] += 1
-        general_stat[parse_line[7]]["file_size"] += int(parse_line[8], base=10)
-        if (i + 1) % 10 == 0:
-            print_stats()
-        i += 1
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
-    main()
+i = 0
+for line in sys.stdin:
+    parse_line = line.split()
+    if parse_line[7] not in general_stat:
+        general_stat[parse_line[7]] = {"number_status": 0, "file_size": 0}
+    general_stat[parse_line[7]]["number_status"] += 1
+    general_stat[parse_line[7]]["file_size"] += int(parse_line[8], base=10)
+    if (i + 1) % 10 == 0:
+        print_stats()
+    i += 1
