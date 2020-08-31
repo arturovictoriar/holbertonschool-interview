e774857bd0df602f083ab4dd2739f90a5e47529a
arvicrin <1231@holbertonschool.com>
2020-08-31 00:38:55 +0000

    Fix the index to get the data

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,35 +13,36 @@
 """
 import sys
 
+general_stat = {"200": 0,
+                "301": 0,
+                "400": 0,
+                "401": 0,
+                "403": 0,
+                "404": 0,
+                "405": 0,
+                "500": 0}
+total_size = {"total": 0}
+
+
+def print_stats():
+    """Print the request statistics"""
+    print("File size: {}".format(total_size["total"]))
+    for status_req in sorted(general_stat.keys()):
+        if general_stat[status_req]:
+            print("{}: {}".format(status_req, general_stat[status_req]))
+
+
+def assign_values(line):
+    """save the values parsed"""
+    try:
+        parse_line = line.split(" ")
+        total_size["total"] += int(parse_line[-1])
+        if parse_line[-2] in general_stat:
+            general_stat[parse_line[-2]] += 1
+    except:
+        pass
+
 if __name__ == "__main__":
-    general_stat = {"200": 0,
-                    "301": 0,
-                    "400": 0,
-                    "401": 0,
-                    "403": 0,
-                    "404": 0,
-                    "405": 0,
-                    "500": 0}
-    total_size = {"total": 0}
-
-    def assign_values(line):
-        """save the values parsed"""
-        try:
-            line = line[:-1]
-            parse_line = line.split(" ")
-            total_size["total"] += int(parse_line[-1])
-            if parse_line[-2] in general_stat:
-                general_stat[parse_line[-2]] += 1
-        except:
-            pass
-
-    def print_stats():
-        """Print the request statistics"""
-        print("File size: {}".format(total_size["total"]))
-        for status_req in sorted(general_stat.keys()):
-            if general_stat[status_req]:
-                print("{}: {}".format(status_req, general_stat[status_req]))
-
     i = 1
     try:
         for line in sys.stdin:
