a449251531d72d3186461354370a8115e7b50cd5
arvicrin <1231@holbertonschool.com>
2020-09-20 21:34:50 +0000

    Fix the number of bytes availables

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,7 +9,18 @@
 
 def validUTF8(data):
     """Check if a set of integers are valid utf-8 format"""
+    num_bytes = 0
     for char_d in data:
-        if (char_d & 0xff) > 127:
+        byte = char_d & 0xff
+        if num_bytes and byte & 0b10000000 and byte & 0b01000000:
             return False
+        num_bytes -= 1
+        num_bytes = max(num_bytes, 0)
+        while (7 - num_bytes) and byte & (1 << (7 - num_bytes)):
+            num_bytes += 1
+        if num_bytes == 1 or num_bytes > 4:
+            return False
+
+    if num_bytes:
+        return False
     return True
