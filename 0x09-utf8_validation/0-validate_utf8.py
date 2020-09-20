3a4e7a524ea37e843960855dd7fbb5a98b184fbd
arvicrin <1231@holbertonschool.com>
2020-09-20 22:09:03 +0000

    Fix the num_bytes

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -13,14 +13,16 @@ def validUTF8(data):
     for char_d in data:
         byte = char_d & 0xff
         if num_bytes:
-            if (byte & 0b10000000 and byte & 0b01000000) or byte & 0b01000000:
+            if (byte >> 6 == 1 or byte >> 6 == 3):
                 return False
             num_bytes -= 1
             continue
-        while (7 - num_bytes) and byte & (1 << (7 - num_bytes)):
+
+        while 7 - num_bytes and byte & (1 << (7 - num_bytes)):
             num_bytes += 1
         if num_bytes == 1 or num_bytes > 4:
             return False
+        num_bytes = max(num_bytes - 1, 0)
 
     if num_bytes:
         return False
