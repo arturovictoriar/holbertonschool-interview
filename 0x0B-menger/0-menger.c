492dca8a28e0c80a705884d5ed22773015cbae31
arvicrin <1231@holbertonschool.com>
2020-10-04 20:27:36 +0000

    Fix the time complexity

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -7,35 +7,17 @@
 */
 void menger(int level)
 {
-	int sponge[1200][1200] = {{0}}, i = 0, j = 0, ori_level = 0, k = 0, l = 0;
-	unsigned int start_margin_x = 0, start_margin_y = 0;
+	int x = 0, y = 0, s_sponge = pow(3, level), space = 0, subx = 0, suby = 0;
 
-	if (level < 0)
-		return;
-
-	ori_level = level;
-	while (level > 0)
+	for (x = 0; x < s_sponge; x++)
 	{
-		for (k = 0; k < pow(3, ori_level - level); k++)
+		for (y = 0; y < s_sponge; y++)
 		{
-			for (l = 0; l < pow(3, ori_level - level); l++)
-			{
-				start_margin_x = (pow(3, level) / 3);
-				start_margin_x += !k ? 1 : (1 + pow(3, level) * k);
-				start_margin_y = (pow(3, level) / 3);
-				start_margin_y += !l ? 1 : (1 + pow(3, level) * l);
-				for (i = start_margin_x; i < start_margin_x + pow(3, level - 1); i++)
-					for (j = start_margin_y; j < start_margin_y + pow(3, level - 1); j++)
-						sponge[i - 1][j - 1] = 1;
-			}
+			for (subx = x, suby = y, space = 0; subx || suby; subx /= 3, suby /= 3)
+				if (subx % 3 == 1 && suby % 3 == 1)
+					space = 1;
+			printf("%c", space ? ' ' : '#');
 		}
-		level--;
-	}
-
-	for (i = 0; i < pow(3, ori_level); i++)
-	{
-		for (j = 0; j < pow(3, ori_level); j++)
-			!sponge[i][j] ? printf("#") : printf(" ");
 		printf("\n");
 	}
 }
