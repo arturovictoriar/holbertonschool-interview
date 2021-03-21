f00c0a9efbee3418950b0d2923c2a32f126ad4e9
arturovictoriar <1231@holbertonschool.com>
2021-03-21 17:57:34 +0000

    Fix time complexity when check avl tree

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,32 +1,10 @@
 #include "binary_trees.h"
 
 /**
-* is_bts - check if a given tree is a bst
-* @tree: head node of a tree
-* Return: 1 is tree is bst or 0 otherwise
-*/
-int is_bts(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (1);
-
-	if (tree->left != NULL && tree->left->n >= tree->n)
-		return (0);
-
-	if (tree->right != NULL && tree->right->n <= tree->n)
-		return (0);
-
-	if (is_bts(tree->left) == 0 || is_bts(tree->right) == 0)
-		return (0);
-
-	return (1);
-}
-
-/**
-* _abs - get the absolute value of a given number
-* @x: value to get the absolute value
-* Return: the absolute value of a given number
-*/
+ * _abs - get the absolute value of a given number
+ * @x: value to get the absolute value
+ * Return: the absolute value of a given number
+ */
 int _abs(int x)
 {
 	if (x < 0)
@@ -35,49 +13,47 @@ int _abs(int x)
 }
 
 /**
-* diff_max_1_height - check if a given tree is differ at most 1 each subtree
-* @tree: head node of a tree
-* Return: the height + 1 of a given tree or 0 if the height of a subtree
-* differ more than 1
-*/
-int diff_max_1_height(const binary_tree_t *tree)
+ * is_tree_valid_avl - check if a given tree is a valid avl
+ * @tree: head node of a tree
+ * @min_tree_v: minimun value of a subtree
+ * @max_tree_v: maximun value of a subtree
+ * @height: height of the tree
+ * Return: 1 on valid avl, 0 otherwise
+ */
+int is_tree_valid_avl(const binary_tree_t *tree, int min_tree_v,
+		int max_tree_v, int *height)
 {
-	int l = 0, r = 0;
+	int height1 = 0, height2 = 0;
 
 	if (tree == NULL)
 		return (1);
 
-	l = diff_max_1_height(tree->left);
-	if (l == 0)
+	if (tree->n <= min_tree_v || tree->n >= max_tree_v)
 		return (0);
 
-	r = diff_max_1_height(tree->right);
-	if (r == 0)
+	if (is_tree_valid_avl(tree->left, min_tree_v, tree->n, &height1) == 0 ||
+			is_tree_valid_avl(tree->right, tree->n, max_tree_v, &height2) == 0)
 		return (0);
 
-	if (_abs(l - r) > 1)
-		return (0);
+	/*select the max value of height*/
+	*height = ((height1 > height2) ? height1 : height2) + 1;
 
-	if (l > r)
-		return (1 + l);
-	return (1 + r);
+	if (_abs(height1 - height2) > 1)
+		return (0);
+	return (1);
 }
 
 /**
-* binary_tree_is_avl - check if a given tree is a valid AVL tree
-* @tree: head node of a tree
-* Return: 1 if a given tree is a valid AVL, 0 otherwise
-*/
+ * binary_tree_is_avl - check if a given tree is a valid AVL tree
+ * @tree: head node of a tree
+ * Return: 1 if a given tree is a valid AVL, 0 otherwise
+ */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
+	int height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (is_bts(tree) == 0)
-		return (0);
-
-	if (diff_max_1_height(tree) == 0)
-		return (0);
-
-	return (1);
+	return (is_tree_valid_avl(tree, INT_MIN, INT_MAX, &height));
 }
