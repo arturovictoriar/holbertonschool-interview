dcf3a26c9202c84dc0d2aa541564ddf677f346f3
arturovictoriar <1231@holbertonschool.com>
2021-04-01 01:37:46 +0000

    Refactorize the code, clean and faster

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,26 +1,6 @@
 #include "holberton.h"
 
 /**
- * loop_recursion_star - get the last star in secuence of a string
- * @s2: string
- * Return: a pointer with the last start in secuence
- */
-char *loop_recursion_star(char *s2)
-{
-	char *next_star = 0;
-
-	if (s2[1] && s2[1] == '*')
-	{
-		next_star = loop_recursion_star(&(s2[1]));
-	}
-	else
-	{
-		next_star = s2;
-	}
-	return (next_star);
-}
-
-/**
  * wildcmp - compares two strings.
  * The special char * can replace any string (including an empty string)
  * @s1: string 1
@@ -29,23 +9,24 @@ char *loop_recursion_star(char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (s1[0] == '\0' && s2[0] == '\0')
-	{
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	}
 
-	if ((s1[0] == '\0' && s2[0] != '\0') || (s1[0] != '\0' && s2[0] == '\0'))
-	{
-		return (0);
-	}
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-	if ((s1[0] == s2[0]) || s2[0] == '*')
+	else if (*s2 == '*')
 	{
-		s2 = loop_recursion_star(s2);
-		if (wildcmp(&(s1[1]), &(s2[0])) || wildcmp(&(s1[1]), &(s2[1])))
-		{
+
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+
+		else if (wildcmp(s1, s2 + 1))
 			return (1);
-		}
+
+		else
+			return (wildcmp(s1 + 1, s2));
+
 	}
 	return (0);
 }
