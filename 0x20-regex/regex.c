01087b8907324b34157aba0503e45cd57dc3dca2
arturovictoriar <1231@holbertonschool.com>
2021-04-15 04:07:16 +0000

    Refactorize the code

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -10,41 +10,17 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
+	int ismatch = 0;
+
 	if (str[0] == '\0' && pattern[0] == '\0')
 		return (1);
-	else if (pattern[0] == '*')
+	if ((str[0] == pattern[0] || pattern[0] == '.') && pattern[1] != '*')
+		return (regex_match(&str[1], &pattern[1]));
+	if (pattern[1] == '*')
 	{
-		if (regex_match(str, &pattern[1]) == 1)
-			return (1);
-	}
-	else if (str[0] == '\0' && pattern[0] == '.')
-	{
-		if (regex_match(str, &pattern[1]) == 1)
-			return (1);
-	}
-	else if (str[0] == pattern[0] && pattern[1] != '*')
-	{
-		if (regex_match(&str[1], &pattern[1]) == 1)
-			return (1);
-	}
-	else if ((str[0] == pattern[0] || pattern[0] == '.') && pattern[1] == '*')
-	{
-		if (regex_match(&str[1], &pattern[0]) == 1)
-			return (1);
-		if (regex_match(&str[1], &pattern[1]) == 1)
-			return (1);
-		if (regex_match(&str[0], &pattern[1]) == 1)
-			return (1);
-	}
-	else if (str[0] != pattern[0] && pattern[1] == '*')
-	{
-		if (regex_match(&str[0], &pattern[1]) == 1)
-			return (1);
-	}
-	else if (str[0] != '\0' && pattern[0] == '.' && pattern[1] != '*')
-	{
-		if (regex_match(&str[1], &pattern[1]) == 1)
-			return (1);
+		if (str[0] != '\0' && (str[0] == pattern[0] || pattern[0] == '.'))
+			ismatch = regex_match(&str[1], pattern);
+		return (regex_match(str, &pattern[2]) || ismatch);
 	}
 	return (0);
 }
